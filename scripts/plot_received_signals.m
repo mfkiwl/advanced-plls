09b0ee3e30f7761ec89577b10bace9eb4197720e
rodrigodelimaf <rdlfresearch@gmail.com>
2025-01-04 19:45:32 -0300

    feat: Added a script and a plot function to evaluate the behavior of the get_csm_data function

--- scripts/plot_received_signals.m
+++
@@ -1,8 +0,0 @@
-addpath(genpath(fullfile(pwd,'..','libs')));
-C_over_N0_dBHz = 45;
-S4 = 0.8;
-tau0 = 0.7;
-simulation_time = 300;
-
-[psi_csm, ~] = get_received_signal(C_over_N0_dBHz,simulation_time,'MFPSM');
-[psi_mfpsm, ps_realization] = get_received_signal(C_over_N0_dBHz,simulation_time,'MFPSM');
