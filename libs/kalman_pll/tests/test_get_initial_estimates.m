c7225020ce1d1d09cc853215bb0a0f13635e4693
rodrigodelimaf <rdlfresearch@gmail.com>
2025-03-05 10:58:07 -0300

    refactor(test_get_initial_estimates): It now uses directly estimated kalman_pll_config struct, instead of creating a dummy one. Removed a useless test. rearranged the TestMethodSetup functions order.

--- libs/kalman_pll/tests/test_get_initial_estimates.m
+++ libs/kalman_pll/tests/test_get_initial_estimates.m
@@ -14,80 +14,76 @@ classdef test_get_initial_estimates < matlab.unittest.TestCase
 %   Email: rdlfresearch@gmail.com
 
     properties
-        DefaultConfig
-        DefaultKalmanPLLConfig
+        default_general_config
+        default_kalman_pll_config
     end
     
     methods(TestMethodSetup)
+        function addParentPath(testCase)
+            full_dir = genpath(fileparts(fileparts(fileparts(mfilename('fullpath')))));
+            addpath(full_dir);
+            testCase.addTeardown(@() rmpath(full_dir));
+        end
         function setDefaults(testCase)
             % Set up a default valid configuration.
-            testCase.DefaultConfig = struct( ...
-                'is_generate_random_initial_estimates', true, ...
-                'initial_states_distributions_boundaries', {{[-pi, pi], [-5, 5], [-0.1, 0.1]}}, ...
-                'real_doppler_profile', [1000, 0.94, 0.03], ... % LOS states length = 3
-                'scintillation_training_data_config', struct('scintillation_model', 'CSM') ...
+            testCase.default_general_config = struct( ...
+                'discrete_wiener_model_config', { {1, 3, 0.01, [0,0,1], 1} }, ...
+                'scintillation_training_data_config', struct('scintillation_model', 'CSM', 'S4', 0.8, 'tau0', 0.7, 'simulation_time', 300, 'sampling_interval', 0.01, 'is_unwrapping_used', false), ...
+                'C_over_N0_array_dBHz', 35, ...
+                'initial_states_distributions_boundaries', { {[-pi,pi], [-5,5], [-0.1,0.1]} }, ...
+                'real_doppler_profile', [0, 1000, 0.94], ...
+                'augmentation_model_initializer', struct('id', 'arfit', 'model_params', struct('model_order', 3)), ...
+                'is_use_cached_settings', false, ...
+                'is_generate_random_initial_estimates', true ...
             );
-            
             % Set up a default kalman_pll_config for model 'CSM'
-            testCase.DefaultKalmanPLLConfig = struct('CSM', struct('var_states_amount', 2, 'var_model_order', 1));
-        end
-        
-        function addParentPath(testCase)
-            parentDir = fileparts(fileparts(mfilename('fullpath')));
-            addpath(parentDir);
-            testCase.addTeardown(@() rmpath(parentDir));
+            cache_file = fullfile(fileparts(mfilename('fullpath')), 'cache_get_initial_estimates');
+            [kalman_pll_config, is_cache_used] = get_cached_kalman_pll_config(testCase.default_general_config, cache_file, false);
+            kalman_pll_config = update_cache(testCase.default_general_config, cache_file, kalman_pll_config, is_cache_used, false);
+            testCase.default_kalman_pll_config = kalman_pll_config;
         end
     end
     
     methods(Test)
         function testValidRandomEstimates(testCase)
             % Test that random estimates are generated when the flag is true.
-            config = testCase.DefaultConfig;
+            config = testCase.default_general_config;
             config.is_generate_random_initial_estimates = true;
             
-            initial_estimates = get_initial_estimates(config, testCase.DefaultKalmanPLLConfig);
+            initial_estimates = get_initial_estimates(config, testCase.default_kalman_pll_config);
             
             % Expected length: LOS (3) + VAR (2*1 = 2) = 5
-            testCase.verifySize(initial_estimates.x_hat_init, [5, 1], 'x_hat_init size mismatch for random estimates.');
-            testCase.verifySize(initial_estimates.P_hat_init, [5, 5], 'P_hat_init size mismatch for random estimates.');
+            testCase.verifySize(initial_estimates.x_hat_init, [6, 1], 'x_hat_init size mismatch for random estimates.');
+            testCase.verifySize(initial_estimates.P_hat_init, [6, 6], 'P_hat_init size mismatch for random estimates.');
         end
         
         function testValidPerfectEstimates(testCase)
             % Test that perfect estimates are generated when the flag is false.
-            config = testCase.DefaultConfig;
+            config = testCase.default_general_config;
             config.is_generate_random_initial_estimates = false;
             
-            initial_estimates = get_initial_estimates(config, testCase.DefaultKalmanPLLConfig);
+            initial_estimates = get_initial_estimates(config, testCase.default_kalman_pll_config);
             
             % Expected length: LOS (3) + VAR (2*1 = 2) = 5
-            testCase.verifySize(initial_estimates.x_hat_init, [5, 1], 'x_hat_init size mismatch for perfect estimates.');
-            testCase.verifySize(initial_estimates.P_hat_init, [5, 5], 'P_hat_init size mismatch for perfect estimates.');
+            testCase.verifySize(initial_estimates.x_hat_init, [6, 1], 'x_hat_init size mismatch for perfect estimates.');
+            testCase.verifySize(initial_estimates.P_hat_init, [6, 6], 'P_hat_init size mismatch for perfect estimates.');
         end
         
         function testBoundaryProfileMismatch(testCase)
             % Set boundaries count different from the length of real_doppler_profile.
-            config = testCase.DefaultConfig;
+            config = testCase.default_general_config;
             config.initial_states_distributions_boundaries = {[-pi, pi], [-5, 5]}; % Only 2 boundaries but real_doppler_profile length is 3.
-            testCase.verifyError(@() get_initial_estimates(config, testCase.DefaultKalmanPLLConfig), ...
+            testCase.verifyError(@() get_initial_estimates(config, testCase.default_kalman_pll_config), ...
                 'get_initial_estimates:BoundaryProfileMismatch');
         end
         
         function testMissingKalmanSubstruct(testCase)
             % Remove the 'CSM' substruct from kalman_pll_config.
-            config = testCase.DefaultConfig;
+            config = testCase.default_general_config;
             config.is_generate_random_initial_estimates = true;
             bad_kalman = struct('TPPSM', struct(), 'none', struct());
             testCase.verifyError(@() get_initial_estimates(config, bad_kalman), ...
                 'get_initial_estimates:MissingScintModelField');
-        end
-        
-        function testMissingKalmanFields(testCase)
-            % Remove a required field (e.g., var_model_order) from the 'CSM' substruct.
-            config = testCase.DefaultConfig;
-            bad_kalman = testCase.DefaultKalmanPLLConfig;
-            bad_kalman.CSM = rmfield(bad_kalman.CSM, 'var_model_order');
-            testCase.verifyError(@() get_initial_estimates(config, bad_kalman), ...
-                'get_initial_estimates:MissingKalmanField');
         end
     end
 end
