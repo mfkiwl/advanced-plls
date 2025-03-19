359ed60582a4850ea5f6405eac101aab08f53341
rodrigodelimaf <rdlfresearch@gmail.com>
2025-03-19 17:27:40 -0300

    Gave up on trying to setup the git hook. I'll handle this manually for now.

--- install-hooks.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This script sets Git to use the custom hooks in the githooks folder.
-
-git config core.hooksPath githooks
-echo "Custom hooks installed. Your repository now uses the hooks in the 'githooks' folder."
