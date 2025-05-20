451760be9479463c6baa5d0bb549492805a0cceb
rodrigodelimaf <rdlfresearch@gmail.com>
2025-05-20 20:59:03 -0300

    Made some tweaks on csm

--- scripts/rodrigo-dissertation/section_signal_model/plot_csm_ts.m
+++ scripts/rodrigo-dissertation/section_signal_model/plot_csm_ts.m
@@ -40,9 +40,9 @@ plot(time,10*log10(abs(weak_ts).^2), 'LineWidth',line_width, 'Color', cmap(1,:))
 hold off;
 grid on;
 grid("minor");
-ylabel('$10 \mathrm{log}10(\rho_1^2 [k])$','interpreter', 'latex');
+ylabel('$10 \mathrm{log}10(\rho_1^2 [k])$ [dB]','interpreter', 'latex');
 legend({'Strong', 'Moderate', 'Weak'}, 'location', 'southwest');
-set(gca, 'FontSize', font_size);
+set(gca, 'FontSize', font_size, 'fontname', 'Times New Roman');
 
 nexttile;
 hold on;
@@ -53,7 +53,7 @@ hold off;
 grid on;
 grid("minor");
 ylabel('$\phi_{\mathrm{I},1}[k]$ [rad]','Interpreter','latex');
-set(gca, 'FontSize', font_size);
+set(gca, 'FontSize', font_size, 'fontname', 'Times New Roman');
 
 nexttile;
 hold on;
@@ -63,17 +63,15 @@ plot(time,phase(weak_ts), 'LineWidth',line_width, 'Color', cmap(1,:));
 hold off;
 grid on;
 grid("minor");
-ylabel('$\mathrm{unwrap}\{\phi_{I,1}[k]\}$ [rad]','Interpreter','latex');
-xlabel('Time [s]');
-set(gca, 'FontSize', font_size);
-exportgraphics(gcf, 'results_pdf/csm_ts.pdf');
+ylabel('$\mathrm{unwrap}\{\phi_{\mathrm{I},1}[k]\}$ [rad]','Interpreter','latex');
+xlabel('Time [s]','Interpreter','latex');
+set(gca, 'FontSize', font_size, 'fontname', 'Times New Roman');
+exportgraphics(gcf, 'results_pdf/csm_ts.pdf', 'ContentType', 'vector');
 
 %% Generate plots - zoomed
 figure('Position',[50,50,1000,600]);
 tiledlayout(3,1,"TileSpacing","tight");
 
-font_size = 14;
-line_width = 1.5;
 nexttile;
 hold on;
 plot(time_zoomed,10*log10(abs(strong_ts(zoom_idx)).^2), 'LineWidth',line_width, 'Color', cmap(3,:));
@@ -82,9 +80,9 @@ plot(time_zoomed,10*log10(abs(weak_ts(zoom_idx)).^2), 'LineWidth',line_width, 'C
 hold off;
 grid on;
 grid("minor");
-ylabel('$10 \mathrm{log}10(\rho_1^2 [k])$','interpreter', 'latex');
+ylabel('$10 \mathrm{log}10(\rho_1^2 [k])$ [dB]','interpreter', 'latex');
 legend({'Strong', 'Moderate', 'Weak'}, 'location', 'southwest');
-set(gca, 'FontSize', font_size);
+set(gca, 'FontSize', font_size, 'fontname', 'Times New Roman');
 
 nexttile;
 hold on;
@@ -95,7 +93,7 @@ hold off;
 grid on;
 grid("minor");
 ylabel('$\phi_{\mathrm{I},1}[k]$ [rad]','Interpreter','latex');
-set(gca, 'FontSize', font_size);
+set(gca, 'FontSize', font_size, 'fontname', 'Times New Roman');
 
 nexttile;
 hold on;
@@ -105,7 +103,7 @@ plot(time_zoomed,phase(weak_ts(zoom_idx)), 'LineWidth',line_width, 'Color', cmap
 hold off;
 grid on;
 grid("minor");
-ylabel('$\mathrm{unwrap}\{\phi_{I,1}[k]\}$ [rad]','Interpreter','latex');
-xlabel('Time [s]');
-set(gca, 'FontSize', font_size);
-exportgraphics(gcf, 'results_pdf/csm_ts_zoom.pdf');
+ylabel('$\mathrm{unwrap}\{\phi_{\mathrm{I},1}[k]\}$ [rad]','Interpreter','latex');
+xlabel('Time [s]','Interpreter','latex');
+set(gca, 'FontSize', font_size, 'fontname', 'Times New Roman');
+exportgraphics(gcf, 'results_pdf/csm_ts_zoom.pdf', 'ContentType', 'vector');
